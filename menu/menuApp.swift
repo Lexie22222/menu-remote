26c7d0ceed9a5f7d896f3e91cd8c0aca44977b14
Lexie22222 <alexisv.bowen@gmail.com>
2023-06-29 00:30:37 -0400

    Initial Commit

---
+++ menu/menuApp.swift
@@ -0,0 +1,17 @@
+//
+//  menuApp.swift
+//  menu
+//
+//  Created by lexie v on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct menuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
